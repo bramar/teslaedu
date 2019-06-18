b0ca661050e8b73e33b39f15034684a75a604b96
Branko Marković <bmarkovic@comtrade.com>
2019-06-18 18:33:32 +0200

    Update

--- scripts/installmoodle.sh
+++ scripts/installmoodle.sh
@@ -32,9 +32,9 @@ sudo mount ${mountpoint}
 
 sudo wget "https://teslaedurepo.blob.core.windows.net/mainrepo/moodle.zip?sp=r&st=2019-06-17T22:00:00Z&se=2029-12-31T23:00:00Z&spr=https&sv=2018-03-28&sig=jukPxF6eYVT3dgP8pvJTnRV%2Bke%2F5h6jvn3cYhCBmRm0%3D&sr=b" -k -O ${mountpoint}/moodle.zip
 cd ${mountpoint}
-unzip moodle.zip
+unzip -qq moodle.zip
 rm moodle.zip
 echo "create database tedu" > /tmp/mysql_setup.sql
-echo "grant all privileges on tedu.* to 'tedu'@'%' identified by 't3duU53r2019'"
+echo "grant all privileges on tedu.* to 'tedu'@'%' identified by 't3duU53r2019'" >> /tmp/mysql_setup.sql
 sudo mysql -u ${mysqlUsername} -p${mysqlPassword} -h ${mysqlHost} < /tmp/mysql_setup.sql
 sudo chown -R www-root:www-root ${mountpoint}/* ${mountpoint}/.*
