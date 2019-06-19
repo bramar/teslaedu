9c7fbd3d73917eb524689cac8b59a202c0fb7c0d
Branko Marković <bmarkovic@comtrade.com>
2019-06-19 12:55:32 +0200

    Error fixing

--- scripts/installmoodle.sh
+++ scripts/installmoodle.sh
@@ -46,4 +46,28 @@ sudo mysql -u ${mysqlUsername} -p${mysqlPassword} -h ${mysqlHost} < /tmp/mysql_s
 
 sudo -u www-data php ${siteroot}/admin/cli/install.php --chmod=777  --lang=en --wwwroot="${siteFQDN}" --dataroot="${mountpoint}" --dbhost="${mysqlHost}" --dbname='tedu' --dbuser="tedu@${mysqlHost}" --dbpass='t3duU53r2019' --fullname='TESLA EDU' --shortname='tedu' --adminuser="${teslaEduAdminUsername}" --adminpass="${teslaEduAdminPassword}" --adminemail="${teslaEduAdminEmail}" --non-interactive --agree-license --allow-unstable || true
 
+sudo cat > /tmp/moodleConfig.sql << EOF
+UPDATE mdl_config SET value ='manual'  WHERE name ='auth';
+UPDATE mdl_config SET value ='manual,guest,self,cohort,ldap,meta'  WHERE name ='enrol_plugins_enabled';
+UPDATE mdl_config SET value ='tedu'  WHERE name ='theme';
+UPDATE mdl_config SET value ='1'  WHERE name ='enablewebservices';
+UPDATE mdl_config SET value ='0'  WHERE name ='autologinguests';
+UPDATE mdl_config SET value ='1'  WHERE name ='autolang';
+UPDATE mdl_config SET value ='en'  WHERE name ='lang';
+UPDATE mdl_config SET value ='1'  WHERE name ='langmenu';
+UPDATE mdl_config SET value ='1'  WHERE name ='defaulthomepage';
+UPDATE mdl_config SET value ='1'  WHERE name ='navshowcategories';
+INSERT INTO mdl_config SET value ='1', name ='navexpandmycourses';
+UPDATE mdl_config SET value ='1'  WHERE name ='enablemobilewebservice';
+UPDATE mdl_config SET value ='Europe/Belgrade'  WHERE name ='timezone';
+INSERT INTO mdl_config SET value ='rest,xmlrpc', name ='webserviceprotocols';
+UPDATE mdl_config SET value ='2'  WHERE name ='grade_report_overview_showtotalsifcontainhidden';
+UPDATE mdl_config SET value ='0,10,11,12,2,4,6,8,13'  WHERE name ='grade_aggregations_visible';
+UPDATE mdl_config SET value ='1'  WHERE name ='grade_aggregateonlygraded';
+UPDATE mdl_config SET value ='ods,txt,xls,xml'  WHERE name ='gradeexport';
+UPDATE mdl_config SET value ='1'  WHERE name ='enablemobilewebservice';
+UPDATE mdl_config SET value ='1'  WHERE name ='authloginviaemail';
+EOF
+sudo mysql -u ${mysqlUsername} -p${mysqlPassword} -h ${mysqlHost} tedu < /tmp/moodleConfig.sql
+
 sudo systemctl reload apache2
